2c83f9918b83edc5414616f68c2d578397ebc6b2
William Woodruff <william@trailofbits.com>
2021-01-19 17:46:26 -0500

    entrypoint: try dropping the GITHUB_WORKSPACE

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ for path in ${paths}; do
   # N.B.: we could run all paths at once with a single winchecksec invocation,
   # but running each independently makes the subsequent operations simpler.
   result=$(mktemp -u)
-  "${WINCHECKSEC}" --json "${GITHUB_WORKSPACE}/${path}" > "${result}"
+  "${WINCHECKSEC}" --json "${path}" > "${result}"
 
   for check in ${checks}; do
     debug "running ${check} on ${path}"
